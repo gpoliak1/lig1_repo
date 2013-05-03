7cb8fc9b0a6aea5183404943ad54621c7e108adc
Gil <gilpoliak9@gmail.com>
2013-05-03 17:32:32 +0300

    Added updated script for vertexLoader

--- OpenGL_Linux/execCompOnly.sh
+++ OpenGL_Linux/execCompOnly.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-g++ -lglut -lGL -lGLEW -lGLU -lpthread -lX11 -c readParse.cpp display.cpp reshape.cpp grid.cpp -pthread
+g++ -lglut -lGL -lGLEW -lGLU -lpthread -lX11 -c readParse.cpp vertexLoader.cpp display.cpp reshape.cpp grid.cpp -pthread
 
-g++ -lglut -lGL -lGLEW -lGLU -lpthread -lX11 -o final readParse.o display.o reshape.o grid.o -pthread
+g++ -lglut -lGL -lGLEW -lGLU -lpthread -lX11 -o final readParse.o vertexLoader.o display.o reshape.o grid.o -pthread
 
 #(exec wmctrl -k on)
 
