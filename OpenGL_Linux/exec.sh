e815f9366e3ddfdb1486c00818f91df07dcfe218
Gil <gilpoliak9@gmail.com>
2013-05-03 06:29:54 -0700

    Added comment to exec script

--- OpenGL_Linux/exec.sh
+++ OpenGL_Linux/exec.sh
@@ -9,3 +9,5 @@ g++ -lglut -lGL -lGLEW -lGLU -lpthread -lX11 -o final readParse.o display.o resh
 (cd /gil/OpenGL_Linux/JavaSocketClient/ && exec xterm cd /gil/OpenGL_Linux/JavaSocketClient/ | javac Form2.java && exec xterm cd /home/gil/Desktop/OpenGL_Linux/JavaSocketClient | java Form2 &)
 
 ./final
+
+# git push https://github.com/lig1/lig1_repo.git master
