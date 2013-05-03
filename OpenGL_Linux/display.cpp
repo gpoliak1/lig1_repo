20c3cffe39b14de25a864ebd7b064fa0d8d8dd16
Gil <gilpoliak9@gmail.com>
2013-05-03 19:33:05 +0300

    ef

--- OpenGL_Linux/display.cpp
+++ OpenGL_Linux/display.cpp
@@ -1,5 +1,31 @@
 #include "display.h"
 
+#define ROT_INC			0.1
+#define factor 			1
+#define PI				3.14159265
+#define RADIUS			5
+#define NUM_OF_ANGLES	10
+void drawGrid(void);
+void setCamera(void);
+void drawNumbers(const char *str1, const char *str2, const char *str3, float pos[3], float color[4], void *font);
+static void (*drawPrimP)(void) = drawGrid;
+void *font = GLUT_BITMAP_8_BY_13;
+static GLfloat g_rotate = 0;
+static GLfloat g_rotInc = ROT_INC;
+float eyeX = 0.0;
+float eyeY = 0.0;
+float eyeZ = -10.0;
+float lookatX = 0.0;
+float lookatY = 0.0;
+float lookatZ = -15.0;
+int multiplierXZ = 0;
+int multiplierYZ = 0;
+float trigSinVals[10];
+float trigCosVals[10];
+
+using namespace std;
+using namespace glm;
+
 void OglDisplay::doOglDisplay(float solidZ, GLuint sphereList, float transparentZ, GLuint cubeList) {
 
     printf("%s%g%s%g%s%g", "\nDISPLAY - Camera now positioned at ", eyeX, ",", eyeY, ",", eyeZ);
