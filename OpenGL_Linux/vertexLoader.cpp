a40e4423e236558df80e5226dc3b9b57b846dfc1
Gil <gilpoliak9@gmail.com>
2013-05-03 20:07:41 +0300

    added vertexLoader

---
+++ OpenGL_Linux/vertexLoader.cpp
@@ -0,0 +1,3 @@
+#include "vertexLoader.h"
+
+using namespace std;
---
+++ OpenGL_Linux/vertexLoader.h
@@ -0,0 +1,11 @@
+#include<map>
+#include<string.h>
+
+#ifndef _VERTEXLOADER_H
+#define _VERTEXLOADER_H
+
+namespace VertexLoader
+{
+
+}
+#endif
