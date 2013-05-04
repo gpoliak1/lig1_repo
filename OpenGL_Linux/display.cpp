44226f1707e16ca27aade27cf7d3944fe82802c3
Gil <gilpoliak9@gmail.com>
2013-05-04 09:18:08 +0300

    Added drawNumbers

--- OpenGL_Linux/display.cpp
+++ OpenGL_Linux/display.cpp
@@ -6,3 +6,23 @@ void OglDisplay::doOglDisplay(float solidZ, GLuint sphereList, float transparent
 	glutSolidSphere(1.0, 20, 16);
 	glFlush();
 }
+
+void drawNumbers(const char *str1, const char *str2, const char *str3, float pos[3], float color[4], void *font)
+{
+    glPushAttrib(GL_LIGHTING_BIT | GL_CURRENT_BIT); // lighting and color mask
+    glDisable(GL_LIGHTING);// need to disable lighting for proper text color
+    glDisable(GL_TEXTURE_2D);
+
+    glColor4fv(color);
+    glRasterPos3fv(pos);
+
+    glutBitmapCharacter(font, *str1);
+    glutBitmapCharacter(font, ',');
+    glutBitmapCharacter(font, *str2);
+    glutBitmapCharacter(font, ',');
+    glutBitmapCharacter(font, *str3);
+
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_LIGHTING);
+    glPopAttrib();
+}
