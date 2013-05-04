4a50e77b8fe6dca864d316526be78003ab186635
Gil <gilpoliak9@gmail.com>
2013-05-04 10:50:38 +0300

    Updated reshape

--- OpenGL_Linux/reshape.cpp
+++ OpenGL_Linux/reshape.cpp
@@ -5,20 +5,34 @@
 
 #define MAX_LINE_LEN 256
 
+float eyeX = 0.0;
+float eyeY = 0.0;
+float eyeZ = -10.0;
+float lookatX = 0.0;
+float lookatY = 0.0;
+float lookatZ = -15.0;
+
+int multiplierXZ = 0;
+int multiplierYZ = 0;
+float trigSinVals[10];
+float trigCosVals[10];
+
 using namespace std;
+using namespace glm;
 
 void Reshape::doReshape(int w, int h) {
 
-	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	if (w <= h) {
-		glOrtho(-1.5, 1.5, -1.5*(GLfloat)h/(GLfloat)w, 1.5*(GLfloat)w/(GLfloat)h, -10.0, 10.0);
-	} else {
-		glOrtho(-1.5*(GLfloat)h/(GLfloat)w, 1.5*(GLfloat)w/(GLfloat)h, -1.5, 1.5, -10.0, 10.0);
-	}
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-        
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity(); /* init projection matrix */
+    gluPerspective( 60.0, (GLdouble)w/(GLdouble)h, 0.1, 40.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+	glScalef(1.0, 1.0, -1.0);
+    gluLookAt(eyeX, eyeY, eyeZ,
+              lookatX, lookatY, lookatZ,
+              0.0, 1.0, 0.0);
+
+	printf("%s","\nreshape");
     
 }
