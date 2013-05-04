6e83e2f9347d035c839a279683abe0d69bf37118
Gil <gilpoliak9@gmail.com>
2013-05-04 17:27:54 +0300

    minor translate

--- OpenGL_LinuxGrid/grid.cpp
+++ OpenGL_LinuxGrid/grid.cpp
@@ -287,11 +287,13 @@ void display(void)
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glScalef(1.0, 1.0, -1.0);
-	glTranslatef(-1,-1,-1);
+	glTranslatef(-5,-5,-5);
 
 	gluLookAt(eyeX, eyeY, eyeZ,lookatX, lookatY, lookatZ,0.0, 1.0, 0.0);
 	glColor3ub( 0, 0, 180 );
-	/*glEnableClientState( GL_VERTEX_ARRAY );
+
+	
+	glEnableClientState( GL_VERTEX_ARRAY );
 	glEnableClientState( GL_TEXTURE_COORD_ARRAY );
 	glEnableClientState( GL_NORMAL_ARRAY );
 	glVertexPointer( 3, GL_FLOAT, sizeof(Vertex), &model[0].position );
@@ -300,7 +302,7 @@ void display(void)
 	glDrawArrays( GL_TRIANGLES, 0, model.size() );
 	glDisableClientState( GL_VERTEX_ARRAY );
 	glDisableClientState( GL_TEXTURE_COORD_ARRAY );
-	glDisableClientState( GL_NORMAL_ARRAY );*/
+	glDisableClientState( GL_NORMAL_ARRAY );
 	OglDisplay::drawGrid();
 	glPopMatrix();
 
