6e18374b05a37d761d466800272b04b7a007e3d6
Gil <gilpoliak9@gmail.com>
2013-05-04 09:42:38 +0300

    added display

--- OpenGL_Linux/display.cpp
+++ OpenGL_Linux/display.cpp
@@ -9,6 +9,7 @@
 void *font = GLUT_BITMAP_8_BY_13;
 static GLfloat g_rotate = 0;
 static GLfloat g_rotInc = ROT_INC;
+static void (*drawPrimP)(void) = drawGrid;
 
 float eyeX = 0.0;
 float eyeY = 0.0;
@@ -25,11 +26,60 @@ float trigCosVals[10];
 using namespace std;
 using namespace glm;
 
+struct Vertex
+{
+    vec3 position;
+    vec2 texcoord;
+    vec3 normal;
+};
+
+struct VertRef
+{
+    VertRef( int v, int vt, int vn )
+        : v(v), vt(vt), vn(vn)
+    { }
+    int v, vt, vn;
+};
+
+
+vector< Vertex > model;
+
 void OglDisplay::doOglDisplay(float solidZ, GLuint sphereList, float transparentZ, GLuint cubeList) {
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glutSolidSphere(1.0, 20, 16);
-	glFlush();
+    printf("%s%g%s%g%s%g", "\nDISPLAY - Camera now positioned at ", eyeX, ",", eyeY, ",", eyeZ);
+    printf("%s%g%s%g%s%g%s", "\nDISPLAY - Looking at ", lookatX, ",", lookatY, ",", lookatZ, "\n");
+
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+    int w = glutGet(GLUT_WINDOW_WIDTH);
+    int h = glutGet(GLUT_WINDOW_HEIGHT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluPerspective( 60.0, (GLdouble)w/(GLdouble)h, 0.1, 40.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+	glScalef(1.0, 1.0, -1.0);
+	glTranslatef(-1,-1,-1);
+
+    gluLookAt(eyeX, eyeY, eyeZ,lookatX, lookatY, lookatZ,0.0, 1.0, 0.0);
+    glColor3ub( 0, 0, 180 );
+    glEnableClientState( GL_VERTEX_ARRAY );
+    glEnableClientState( GL_TEXTURE_COORD_ARRAY );
+    glEnableClientState( GL_NORMAL_ARRAY );
+    glVertexPointer( 3, GL_FLOAT, sizeof(Vertex), &model[0].position );
+    glTexCoordPointer( 2, GL_FLOAT, sizeof(Vertex), &model[0].texcoord );
+    glNormalPointer( GL_FLOAT, sizeof(Vertex), &model[0].normal );
+    glDrawArrays( GL_TRIANGLES, 0, model.size() );
+    glDisableClientState( GL_VERTEX_ARRAY );
+    glDisableClientState( GL_TEXTURE_COORD_ARRAY );
+    glDisableClientState( GL_NORMAL_ARRAY );
+	(*drawPrimP)();
+    glPopMatrix();
+
+    glutSwapBuffers();
 }
 
 void drawNumbers(const char *str1, const char *str2, const char *str3, float pos[3], float color[4], void *font)
