556bc7f4b06041886104ef5302d69a33dacf6986
Gil <gilpoliak9@gmail.com>
2013-05-04 11:27:54 +0300

    added loadTrigValues

--- OpenGL_Linux/grid.cpp
+++ OpenGL_Linux/grid.cpp
@@ -12,6 +12,11 @@ float WINPOSX;
 float WINPOSY;
 
 #define CONFIG		"test.cfg"
+#define ROT_INC			0.1
+#define factor 			1
+#define PI				3.14159265
+#define RADIUS			5
+#define NUM_OF_ANGLES	10
 
 static float solidZ = MAXZ;
 static float transparentZ = MINZ;
@@ -138,6 +143,35 @@ void keyboard(unsigned char key, int x, int y)
 
 	XFlush(display);
 	XCloseDisplay(display);
+}
+
+void loadTrigValsIntoArr(void) {
+
+    trigCosVals[0] = (RADIUS * cos(360*PI/180));
+    trigSinVals[0] = (RADIUS * sin(360*PI/180));
+    trigCosVals[1] = (RADIUS * cos(36*PI/180));
+    trigSinVals[1] = (RADIUS * sin(36*PI/180));
+    trigCosVals[2] = (RADIUS * cos(72*PI/180));
+    trigSinVals[2] = (RADIUS * sin(72*PI/180));
+    trigCosVals[3] = (RADIUS * cos(108*PI/180));
+    trigSinVals[3] = (RADIUS * sin(108*PI/180));
+    trigCosVals[4] = (RADIUS * cos(144*PI/180));
+    trigSinVals[4] = (RADIUS * sin(144*PI/180));
+    trigCosVals[5] = (RADIUS * cos(180*PI/180));
+    trigSinVals[5] = (RADIUS * sin(180*PI/180));
+    trigCosVals[6] = (RADIUS * cos(216*PI/180));
+    trigSinVals[6] = (RADIUS * sin(216*PI/180));
+    trigCosVals[7] = (RADIUS * cos(252*PI/180));
+    trigSinVals[7] = (RADIUS * sin(252*PI/180));
+    trigCosVals[8] = (RADIUS * cos(288*PI/180));
+    trigSinVals[8] = (RADIUS * sin(288*PI/180));
+    trigCosVals[9] = (RADIUS * cos(324*PI/180));
+    trigSinVals[9] = (RADIUS * sin(324*PI/180));
+    int i;
+    for (i=0; i<NUM_OF_ANGLES; i++) {
+        printf("\nCos: %f", trigCosVals[i]);
+        printf("\nSin: %f", trigSinVals[i]);
+    }
 }
 
 void mouse(int btn, int state, int x, int y)
