915317ccfa7d3f336006e17a274a019a6fb136d9
Gil <gilpoliak9@gmail.com>
2013-05-07 09:08:49 -0700

    Added additional examples

--- OpenGL_LinuxGrid/examples/lightlab.c
+++ OpenGL_LinuxGrid/examples/lightlab.c
@@ -16,12 +16,24 @@ enum {
 GLfloat red_light[] = {1.0, 0.0, 0.0, 1.0}; 
 GLfloat green_light[] = {0.0, 1.0, 0.0, 1.0};
 GLfloat white_light[] = {1.0, 1.0, 1.0, 1.0};
-GLfloat left_light_position[] = {-1.0, 0.0, 1.0, 0.0};
-GLfloat right_light_position[] = {1.0, 0.0, 1.0, 0.0};
+GLfloat blue_light[] = {0.0, 0.0, 1.0, 1.0};
+GLfloat light_1_position[] = {-1.0, 0.0, 1.0, 0.0};
+GLfloat light_2_position[] = {1.0, 0.0, 1.0, 0.0};
+GLfloat light_3_position[] = {0.0, 0.0, 1.0, 0.0};
+GLfloat red_plastic_ambient[] = {0.2, 0.2, 0.2}; 
+GLfloat red_plastic_diffuse[] = {0.7, 0.7, 0.7};
+GLfloat red_plastic_specular[] = {0.4, 0.4, 0.4};
+GLfloat red_plastic_shininess = 100.0;
+
+/*GLfloat red_light[] = {1.0, 0.0, 0.0, 1.0}; 
+GLfloat green_light[] = {0.0, 1.0, 0.0, 1.0};
+GLfloat white_light[] = {1.0, 1.0, 1.0, 1.0};
+GLfloat light_1_position[] = {-1.0, 0.0, 1.0, 0.0};
+GLfloat light_2_position[] = {1.0, 0.0, 1.0, 0.0};
 GLfloat red_plastic_ambient[] = {0.0, 0.0, 0.0}; 
 GLfloat red_plastic_diffuse[] = {0.5, 0.0, 0.0};
 GLfloat red_plastic_specular[] = {0.7, 0.6, 0.6};
-GLfloat red_plastic_shininess = 32.0;
+GLfloat red_plastic_shininess = 32.0;*/
 
 int shade_model = GL_SMOOTH;
 
@@ -35,28 +47,29 @@ display(void)
   	glScalef(1.3, 1.3, 1.3);
   	glRotatef(20.0, 1.0, 0.0, 0.0);
   	glPushMatrix();
-  		glTranslatef(-0.65, 0.7, 0.0);
-  		glRotatef(90.0, 1.0, 0.0, 0.0);
+  		glTranslatef(-0.65, 0.7, 0.0);//glTranslatef(-0.65, 0.7, 0.0);
+  		glRotatef(100.0, 1.0, 0.0, 0.0);//glRotatef(90.0, 1.0, 0.0, 0.0);
   		glCallList(TORUS_MATERIAL);
   		glutSolidTorus(0.275, 0.85, 10, 15);
+
   	glPopMatrix();
   glPopMatrix();
 
   glPushAttrib(GL_ENABLE_BIT);
   glDisable(GL_DEPTH_TEST);
   glDisable(GL_LIGHTING);
-  glMatrixMode(GL_PROJECTION);
+  //glMatrixMode(GL_PROJECTION);
 
-  glPushMatrix();
-  glLoadIdentity();
-  gluOrtho2D(0, 3000, 0, 3000);
-  glMatrixMode(GL_MODELVIEW);
+  //glPushMatrix();
+  //glLoadIdentity();
+  //gluOrtho2D(0, 3000, 0, 3000);
+  //glMatrixMode(GL_MODELVIEW);
 
-  glPushMatrix();
-  	glLoadIdentity();
-  	glPopMatrix();
-  	glMatrixMode(GL_PROJECTION);
-  glPopMatrix();
+  //glPushMatrix();
+  //	glLoadIdentity();
+  //	glPopMatrix();
+  //	glMatrixMode(GL_PROJECTION);
+  //glPopMatrix();
   glPopAttrib();
 
   glutSwapBuffers();
@@ -73,17 +86,29 @@ main(int argc, char **argv)
   glutCreateWindow("Lighting Laboratory");
   glutDisplayFunc(display);
 
-  glLightfv(GL_LIGHT0, GL_POSITION, left_light_position);
+  glLightfv(GL_LIGHT0, GL_POSITION, light_1_position);
   glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);
-  glLightfv(GL_LIGHT1, GL_POSITION, right_light_position);
+  glLightfv(GL_LIGHT1, GL_POSITION, light_2_position);
   glLightfv(GL_LIGHT1, GL_SPECULAR, white_light);
+  glLightfv(GL_LIGHT2, GL_POSITION, light_3_position);
+  glLightfv(GL_LIGHT2, GL_SPECULAR, white_light);
 
   glEnable(GL_LIGHT0);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, red_light);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);//glLightfv(GL_LIGHT0, GL_DIFFUSE, red_light);
   //glutPostRedisplay();
+/*GLfloat spot_direction[] = {1.0, -1.0, 1.0};
+glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 90.0);
+glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
+glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, 2.0);
+glEnable(GL_LIGHT0);*/
+
 
   glEnable(GL_LIGHT1);
-    glLightfv(GL_LIGHT1, GL_DIFFUSE, green_light);
+  glLightfv(GL_LIGHT1, GL_DIFFUSE, green_light);//glLightfv(GL_LIGHT1, GL_DIFFUSE, green_light);
+  //glutPostRedisplay();
+
+  glEnable(GL_LIGHT2);
+  glLightfv(GL_LIGHT2, GL_DIFFUSE, blue_light);//glLightfv(GL_LIGHT1, GL_DIFFUSE, green_light);
   //glutPostRedisplay();
 
   glNewList(TORUS_MATERIAL, GL_COMPILE);
