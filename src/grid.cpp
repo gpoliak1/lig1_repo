b12ace14a495e63c76d9ef69fda6fbdf4d11be47
Gil <gil@ubuntu.(none)>
2013-05-18 02:53:01 +0300

    Forward and Backward motion corrected

--- src/grid.cpp
+++ src/grid.cpp
@@ -18,120 +18,109 @@ using namespace std;
 using namespace glm;
 
 void setCamera(void);
-static GLfloat g_rotate = 0;
-static GLfloat g_rotInc = ROT_INC;
 
+//General values
 float eyeX = 0.0;
 float eyeY = 0.0;
 float eyeZ = 15.0;
 float lookatX = 0.0;
 float lookatY = 0.0;
 float lookatZ = -15.0;
-
 int multiplierXZ = 0;
 int multiplierYZ = 0;
 
-struct Vertex
-{
+//Basic Config
+bool gridOn = false;
+
+struct Vertex {
 	vec3 position;
 	vec2 texcoord;
 	vec3 normal;
 };
 
-struct VertRef
-{
-	VertRef( int v, int vt, int vn )
-		: v(v), vt(vt), vn(vn)
-	{ }
+struct VertRef {
+	VertRef(int v, int vt, int vn) :
+			v(v), vt(vt), vn(vn) {
+	}
 	int v, vt, vn;
 };
 
+vector<Vertex> models[2];
 
-vector< Vertex > models[2];
+vector<Vertex> LoadOBJ(istream& in) {
+	vector<Vertex> verts;
 
-vector< Vertex > LoadOBJ( istream& in )
-{
-	vector< Vertex > verts;
-
-	vector< vec4 > positions( 1, vec4( 0, 0, 0, 0 ) );
-	vector< vec3 > texcoords( 1, vec3( 0, 0, 0 ) );
-	vector< vec3 > normals( 1, vec3( 0, 0, 0 ) );
+	vector<vec4> positions(1, vec4(0, 0, 0, 0));
+	vector<vec3> texcoords(1, vec3(0, 0, 0));
+	vector<vec3> normals(1, vec3(0, 0, 0));
 	string lineStr;
-	while( getline( in, lineStr ) )
-	{
-		istringstream lineSS( lineStr );
+	while (getline(in, lineStr)) {
+		istringstream lineSS(lineStr);
 		string lineType;
 		lineSS >> lineType;
 
 		// vertex
-		if( lineType == "v" )
-		{
+		if (lineType == "v") {
 			float x = 0, y = 0, z = 0, w = 1;
 			lineSS >> x >> y >> z >> w;
-			positions.push_back( vec4( x, y, z, w ) );
+			positions.push_back(vec4(x, y, z, w));
 		}
 
 		// texture
-		if( lineType == "vt" )
-		{
+		if (lineType == "vt") {
 			float u = 0, v = 0, w = 0;
 			lineSS >> u >> v >> w;
-			texcoords.push_back( vec3( u, v, w ) );
+			texcoords.push_back(vec3(u, v, w));
 		}
 
 		// normal
-		if( lineType == "vn" )
-		{
+		if (lineType == "vn") {
 			float i = 0, j = 0, k = 0;
 			lineSS >> i >> j >> k;
-			normals.push_back( normalize( vec3( i, j, k ) ) );
+			normals.push_back(normalize(vec3(i, j, k)));
 		}
 
 		// polygon
-		if( lineType == "f" )
-		{
-			vector< VertRef > refs;
+		if (lineType == "f") {
+			vector<VertRef> refs;
 			string refStr;
-			while( lineSS >> refStr )
-			{
-				istringstream ref( refStr );
+			while (lineSS >> refStr) {
+				istringstream ref(refStr);
 				string vStr, vtStr, vnStr;
-				getline( ref, vStr, '/' );
-				getline( ref, vtStr, '/' );
-				getline( ref, vnStr, '/' );
-				int v = atoi( vStr.c_str() );
-				int vt = atoi( vtStr.c_str() );
-				int vn = atoi( vnStr.c_str() );
-				v  = (  v >= 0 ?  v : positions.size() +  v );
-				vt = ( vt >= 0 ? vt : texcoords.size() + vt );
-				vn = ( vn >= 0 ? vn : normals.size()   + vn );
-				refs.push_back( VertRef( v, vt, vn ) );
+				getline(ref, vStr, '/');
+				getline(ref, vtStr, '/');
+				getline(ref, vnStr, '/');
+				int v = atoi(vStr.c_str());
+				int vt = atoi(vtStr.c_str());
+				int vn = atoi(vnStr.c_str());
+				v = (v >= 0 ? v : positions.size() + v);
+				vt = (vt >= 0 ? vt : texcoords.size() + vt);
+				vn = (vn >= 0 ? vn : normals.size() + vn);
+				refs.push_back(VertRef(v, vt, vn));
 			}
 
-			if( refs.size() < 3 )
-			{
+			if (refs.size() < 3) {
 				// error, skip
 				continue;
 			}
 
 			// triangulate, assuming n>3-gons are convex and coplanar
 			VertRef* p[3] = { &refs[0], NULL, NULL };
-			for( size_t i = 1; i+1 < refs.size(); ++i )
-			{
-				p[1] = &refs[i+0];
-				p[2] = &refs[i+1];
+			for (size_t i = 1; i + 1 < refs.size(); ++i) {
+				p[1] = &refs[i + 0];
+				p[2] = &refs[i + 1];
 
-				vec3 U( positions[ p[1]->v ] - positions[ p[0]->v ] );
-				vec3 V( positions[ p[2]->v ] - positions[ p[0]->v ] );
-				vec3 faceNormal = normalize( cross( U, V ) );
+				vec3 U(positions[p[1]->v] - positions[p[0]->v]);
+				vec3 V(positions[p[2]->v] - positions[p[0]->v]);
+				vec3 faceNormal = normalize(cross(U, V));
 
-				for( size_t j = 0; j < 3; ++j )
-				{
+				for (size_t j = 0; j < 3; ++j) {
 					Vertex vert;
-					vert.position = vec3( positions[ p[j]->v ] );
-					vert.texcoord = vec2( texcoords[ p[j]->vt ] );
-					vert.normal   = ( p[j]->vn != 0 ? normals[ p[j]->vn ] : faceNormal );
-					verts.push_back( vert );
+					vert.position = vec3(positions[p[j]->v]);
+					vert.texcoord = vec2(texcoords[p[j]->vt]);
+					vert.normal = (
+							p[j]->vn != 0 ? normals[p[j]->vn] : faceNormal);
+					verts.push_back(vert);
 				}
 			}
 		}
@@ -140,52 +129,103 @@ vector< Vertex > LoadOBJ( istream& in )
 	return verts;
 }
 
-void myKey(unsigned char k, int x, int y)
-{
-	float resX;
-	float resY;
+void moveForward() {
+	if ((lookatX - eyeX) > 0) {
+		eyeX += 1.0;
+		lookatX += 1.0;
+	} else if ((lookatX - eyeX) < 0) {
+		eyeX -= 1.0;
+		lookatX -= 1.0;
+	}
+
+	if ((lookatY - eyeY) > 0) {
+		eyeY += 1.0;
+		lookatY += 1.0;
+	} else if ((lookatY - eyeY) < 0) {
+		eyeY -= 1.0;
+		lookatY -= 1.0;
+	}
+
+	if ((lookatZ - eyeZ) > 0) {
+		eyeZ += 1.0;
+		lookatZ += 1.0;
+	} else if ((lookatZ - eyeZ) < 0) {
+		eyeZ -= 1.0;
+		lookatZ -= 1.0;
+	}
+}
+
+void moveBackward() {
+	if ((lookatX - eyeX) > 0) {
+		eyeX -= 1.0;
+		lookatX -= 1.0;
+	} else if ((lookatX - eyeX) < 0) {
+		eyeX += 1.0;
+		lookatX += 1.0;
+	}
+
+	if ((lookatY - eyeY) > 0) {
+		eyeY -= 1.0;
+		lookatY -= 1.0;
+	} else if ((lookatY - eyeY) < 0) {
+		eyeY += 1.0;
+		lookatY += 1.0;
+	}
+
+	if ((lookatZ - eyeZ) > 0) {
+		eyeZ -= 1.0;
+		lookatZ -= 1.0;
+	} else if ((lookatZ - eyeZ) < 0) {
+		eyeZ += 1.0;
+		lookatZ += 1.0;
+	}
+}
+
+void keyboard(unsigned char k, int x, int y) {
 
 	switch (k) {
 	case 'q':
 	case 'Q':
 		exit(0);
 		break;
-	case 'r':
-		eyeX -= 1.0;
-		lookatX -= 1.0;
 
+//	case 'r':
+////		eyeX -= 1.0;
+////		lookatX -= 1.0;
+////
+////		setCamera();
+//		break;
+//	case 'l':
+////		eyeX += 1.0;
+////		lookatX += 1.0;
+////
+////		setCamera();
+////		break;
+//	case 'u':
+////		eyeY += 1.0;
+////		lookatY += 1.0;
+////
+////		setCamera();
+////		break;
+//	case 'd':
+////		eyeY -= 1.0;
+////		lookatY -= 1.0;
+////
+////		setCamera();
+////		break;
+
+	case 'f':
+
+		moveForward();
 		setCamera();
 		break;
-	case 'l':
-		eyeX += 1.0;
-		lookatX += 1.0;
 
+	case 'b':
+
+		moveBackward();
 		setCamera();
 		break;
-	case 'u':
-		eyeY += 1.0;
-		lookatY += 1.0;
 
-		setCamera();
-		break;
-	case 'd':
-		eyeY -= 1.0;
-		lookatY -= 1.0;
-
-		setCamera();
-		break;
-	case 'i':
-		eyeZ += 1.0;
-		lookatZ += 1.0;
-
-		setCamera();
-		break;
-	case 'o':
-		eyeZ -= 1.0;
-		lookatZ -= 1.0;
-
-		setCamera();
-		break;
 	case 'm':
 		//looks immediately left
 
@@ -212,7 +252,7 @@ void myKey(unsigned char k, int x, int y)
 		lookatX = Grid::trigSinVals[multiplierXZ] + eyeX;
 		lookatZ = Grid::trigCosVals[multiplierXZ] + eyeZ;
 
-		printf("\nAngle XZ: %d",multiplierYZ*36);
+		printf("\nAngle XZ: %d", multiplierYZ * 36);
 
 		setCamera();
 		break;
@@ -228,7 +268,7 @@ void myKey(unsigned char k, int x, int y)
 		lookatY = Grid::trigSinVals[multiplierYZ] + eyeY;
 		lookatZ = Grid::trigCosVals[multiplierYZ] + eyeZ;
 
-		printf("\nAngle YZ: %d",multiplierYZ*36);
+		printf("\nAngle YZ: %d", multiplierYZ * 36);
 
 		setCamera();
 		break;
@@ -252,7 +292,6 @@ void myKey(unsigned char k, int x, int y)
 	}
 }
 
-
 void setCamera(void) {
 	int w = glutGet(GLUT_WINDOW_WIDTH);
 	int h = glutGet(GLUT_WINDOW_HEIGHT);
@@ -261,39 +300,40 @@ void setCamera(void) {
 	glLoadIdentity();
 	glScalef(1.0, 1.0, -1.0);
 	//glScalef(1.0, 1.0, -1.0);
-	gluPerspective( 60.0, (GLdouble)w/(GLdouble)h, 0.1, 40.0);
+	gluPerspective(60.0, (GLdouble) w / (GLdouble) h, 0.1, 40.0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(eyeX, eyeY, eyeZ,lookatX, lookatY, lookatZ,0.0, 1.0, 0.0);
+	gluLookAt(eyeX, eyeY, eyeZ, lookatX, lookatY, lookatZ, 0.0, 1.0, 0.0);
 
 	glutPostRedisplay();
 }
 
-void display(void)
-{
-	printf("%s%g%s%g%s%g", "\nDISPLAY - Camera now positioned at ", eyeX, ",", eyeY, ",", eyeZ);
-	printf("%s%g%s%g%s%g%s", "\nDISPLAY - Looking at ", lookatX, ",", lookatY, ",", lookatZ, "\n");
+void display(void) {
+	printf("%s%g%s%g%s%g", "\nDISPLAY - Camera now positioned at ", eyeX, ",",
+			eyeY, ",", eyeZ);
+	printf("%s%g%s%g%s%g%s", "\nDISPLAY - Looking at ", lookatX, ",", lookatY,
+			",", lookatZ, "\n");
 
-	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	int w = glutGet(GLUT_WINDOW_WIDTH);
 	int h = glutGet(GLUT_WINDOW_HEIGHT);
 
-	glEnable( GL_DEPTH_TEST );
+	glEnable(GL_DEPTH_TEST);
 
 	// set up "headlamp"-like light
-	glShadeModel( GL_SMOOTH );
-	glEnable( GL_COLOR_MATERIAL );
-	glColorMaterial( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE );
-	glEnable( GL_LIGHTING );
-	glEnable( GL_LIGHT0 );
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_COLOR_MATERIAL);
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
 
 	GLfloat position[] = { 0, 0, 1, 0 };
-	glLightfv( GL_LIGHT0, GL_POSITION, position );
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective( 60.0, (GLdouble)w/(GLdouble)h, 0.1, 40.0);
+	gluPerspective(60.0, (GLdouble) w / (GLdouble) h, 0.1, 40.0);
 
 	gluLookAt(eyeX, eyeY, eyeZ, lookatX, lookatY, lookatZ, 0.0, 1.0, 0.0);
 
@@ -302,121 +342,117 @@ void display(void)
 	glLoadIdentity();
 	glScalef(1.0, 1.0, 1.0);
 	glTranslatef(-1, -1, -1);
-	glColor3ub( 0, 0, 180 );
-	OglDisplay::drawGrid();
+	glColor3ub(0, 0, 180);
+
+	if (gridOn) {
+		OglDisplay::drawGrid();
+	}
 
 	/** stone */
 	int j;
 	//glColor3ub( 180, 0, 0 );
 	//glTranslatef(10, -1, -1);
 	for (j = 0; j < 1; j++) {
-	glColor3ub( 180, 0, 0 );
-	glTranslatef(2, -1, -1);
+		glColor3ub(180, 0, 0);
+		glTranslatef(2, -1, -1);
 
-	glEnableClientState( GL_VERTEX_ARRAY );
-	glEnableClientState( GL_TEXTURE_COORD_ARRAY );
-	glEnableClientState( GL_NORMAL_ARRAY );
-	glVertexPointer( 3, GL_FLOAT, sizeof(Vertex), &models[j][0].position );
-	glTexCoordPointer( 2, GL_FLOAT, sizeof(Vertex), &models[j][0].texcoord );
-	glNormalPointer( GL_FLOAT, sizeof(Vertex), &models[j][0].normal );
-	glDrawArrays( GL_TRIANGLES, 0, models[j].size() );
-	glDisableClientState( GL_VERTEX_ARRAY );
-	glDisableClientState( GL_TEXTURE_COORD_ARRAY );
-	glDisableClientState( GL_NORMAL_ARRAY );
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glEnableClientState(GL_NORMAL_ARRAY);
+		glVertexPointer(3, GL_FLOAT, sizeof(Vertex), &models[j][0].position);
+		glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), &models[j][0].texcoord);
+		glNormalPointer(GL_FLOAT, sizeof(Vertex), &models[j][0].normal);
+		glDrawArrays(GL_TRIANGLES, 0, models[j].size());
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+		glDisableClientState(GL_NORMAL_ARRAY);
 	}
 
 	/** stone 2 */
 	/*glColor3ub( 0, 180, 0 );
-	glTranslatef(-5, -1, -1);
+	 glTranslatef(-5, -1, -1);
 
-	glEnableClientState( GL_VERTEX_ARRAY );
-	glEnableClientState( GL_TEXTURE_COORD_ARRAY );
-	glEnableClientState( GL_NORMAL_ARRAY );
-	glVertexPointer( 3, GL_FLOAT, sizeof(Vertex), &models[j][0].position );
-	glTexCoordPointer( 2, GL_FLOAT, sizeof(Vertex), &models[j][0].texcoord );
-	glNormalPointer( GL_FLOAT, sizeof(Vertex), &models[j][0].normal );
-	glDrawArrays( GL_TRIANGLES, 0, models[j].size() );
-	glDisableClientState( GL_VERTEX_ARRAY );
-	glDisableClientState( GL_TEXTURE_COORD_ARRAY );
-	glDisableClientState( GL_NORMAL_ARRAY );*/
-	
+	 glEnableClientState( GL_VERTEX_ARRAY );
+	 glEnableClientState( GL_TEXTURE_COORD_ARRAY );
+	 glEnableClientState( GL_NORMAL_ARRAY );
+	 glVertexPointer( 3, GL_FLOAT, sizeof(Vertex), &models[j][0].position );
+	 glTexCoordPointer( 2, GL_FLOAT, sizeof(Vertex), &models[j][0].texcoord );
+	 glNormalPointer( GL_FLOAT, sizeof(Vertex), &models[j][0].normal );
+	 glDrawArrays( GL_TRIANGLES, 0, models[j].size() );
+	 glDisableClientState( GL_VERTEX_ARRAY );
+	 glDisableClientState( GL_TEXTURE_COORD_ARRAY );
+	 glDisableClientState( GL_NORMAL_ARRAY );*/
+
 	glPopMatrix();
 	glutSwapBuffers();
 }
 
-
-void myReshape(int w, int h)
-{
+void reshape(int w, int h) {
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity(); /* init projection matrix */
-	gluPerspective( 60.0, (GLdouble)w/(GLdouble)h, 0.1, 40.0);
+	gluPerspective(60.0, (GLdouble) w / (GLdouble) h, 0.1, 40.0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glScalef(1.0, 1.0, -1.0);
-	gluLookAt(eyeX, eyeY, eyeZ,
-		lookatX, lookatY, lookatZ,
-		0.0, 1.0, 0.0);
+	gluLookAt(eyeX, eyeY, eyeZ, lookatX, lookatY, lookatZ, 0.0, 1.0, 0.0);
 
-	printf("%s","\nreshape");
+	printf("%s", "\nreshape");
 }
 
-void myMouse(int btn, int state, int x, int y)
-{
+void mouse(int btn, int state, int x, int y) {
 
 }
 
-void myIdleFunc(void) {
+void idle(void) {
 
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	//from objloader
-	ifstream stone("test.obj" );
-	ifstream stone2("test.obj" );
-	ifstream stone3("test.obj" );
-	ifstream stone4("test.obj" );
-	ifstream stone5("test.obj" );
+	ifstream stone("test.obj");
+	ifstream stone2("test.obj");
+	ifstream stone3("test.obj");
+	ifstream stone4("test.obj");
+	ifstream stone5("test.obj");
 	models[0] = LoadOBJ(stone);
 	models[1] = LoadOBJ(stone2);
 	models[2] = LoadOBJ(stone3);
 	models[3] = LoadOBJ(stone4);
 	models[4] = LoadOBJ(stone5);
 
-	glutInit( &argc, argv );
+	glutInit(&argc, argv);
 	Util::loadTrigValsIntoArr();
-	glutInitDisplayMode( GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE );
-	glutInitWindowSize( 640, 480 );
-	glutReshapeFunc(myReshape);
-	glutCreateWindow( "3D GRID" );
-	glutDisplayFunc( display );
-	glutIdleFunc(myIdleFunc);
-	glutKeyboardFunc(myKey);
-	glutMouseFunc(myMouse);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(640, 480);
+	glutReshapeFunc(reshape);
+	glutCreateWindow("3D GRID");
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutKeyboardFunc(keyboard);
+	glutMouseFunc(mouse);
 
 	/*glEnable( GL_DEPTH_TEST );
 
-	// set up "headlamp"-like light
-	glShadeModel( GL_SMOOTH );
-	glEnable( GL_COLOR_MATERIAL );
-	glColorMaterial( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE ) ;
-	glEnable( GL_LIGHTING );
-	glEnable( GL_LIGHT0 );*/
+	 // set up "headlamp"-like light
+	 glShadeModel( GL_SMOOTH );
+	 glEnable( GL_COLOR_MATERIAL );
+	 glColorMaterial( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE ) ;
+	 glEnable( GL_LIGHTING );
+	 glEnable( GL_LIGHT0 );*/
 
 	//glMatrixMode(GL_PROJECTION);
 	//gluLookAt(-10, 0, 5, 0, 0, 5, 0.0, 1.0, 0.0);
-
-	glMatrixMode( GL_MODELVIEW );
+	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glScalef(1.0, 1.0, -1.0);
-	glTranslatef(-1,-1,-1);
+	glTranslatef(-1, -1, -1);
 
 	/*GLfloat position[] = { 0, 0, 1, 0 };
-	glLightfv( GL_LIGHT0, GL_POSITION, position );*/
+	 glLightfv( GL_LIGHT0, GL_POSITION, position );*/
 
-	glPolygonMode( GL_FRONT, GL_FILL );
-	glPolygonMode( GL_BACK, GL_LINE );
+	glPolygonMode(GL_FRONT, GL_FILL);
+	glPolygonMode(GL_BACK, GL_LINE);
 
 	glutMainLoop();
 
