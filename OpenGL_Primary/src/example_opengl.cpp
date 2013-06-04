dcbf0e9d97dc21d389da7fdafb303960f3054381
Gil <gil@gil-Inspiron-N5010.(none)>
2013-06-04 15:30:04 +0300

    Concise loading of multiple textures

--- OpenGL_Primary/src/example_opengl.cpp
+++ OpenGL_Primary/src/example_opengl.cpp
@@ -46,21 +46,17 @@
 #include <GL/glut.h>
 #include <stdio.h>
 
+#define NUM_OF_TEXTURES		2
+
 size_t u2 = 1;
 size_t v2 = 1;
 double u3;
 double v3;
-std::vector<unsigned char> bricks;
-std::vector<unsigned char> wood;
+
+std::vector<unsigned char> preArr;
 unsigned width, height;
 
 //General values
-//float eyeX = 4.0;
-//float eyeY = 4.0;
-//float eyeZ = 2.0;
-//float lookatX = 0.0;
-//float lookatY = 0.0;
-//float lookatZ = -15.0;
 float eyeX = 0.5;
 float eyeY = 0.5;
 float eyeZ = 2.0;
@@ -68,71 +64,50 @@ float lookatX = 0.5;
 float lookatY = 0.5;
 float lookatZ = -15.0;
 
-static GLuint texName[2];
+static GLuint texName[NUM_OF_TEXTURES];
 
-std::vector<unsigned char> makeBricksPowerOfTwo(size_t& u2, size_t& v2) {
-
-	// Texture size must be power of two for the primitive OpenGL version this is written for. Find next power of two.
-	while (u2 < width)
-		u2 *= 2;
-	while (v2 < height)
-		v2 *= 2;
-	// Make power of two version of the image.
-	std::vector<unsigned char> bricks2(u2 * v2 * 4);
-	unsigned origWidth = width;
-	unsigned origHeight = height;
+std::vector<unsigned char> makePowerOfTwo(size_t& u2, size_t& v2,
+		std::vector<unsigned char>& incArr,
+		std::vector<unsigned char>& outArr) {
 
 	//for cropping, the top is lowered for height, right border moves left for width
 	for (size_t y = 0; y < height; y++)
 		for (size_t x = 0; x < width; x++)
 			for (size_t c = 0; c < 4; c++) {
-				bricks2[4 * u2 * y + 4 * x + c] = bricks[4 * width * y + 4 * x
+				outArr[4 * u2 * y + 4 * x + c] = incArr[4 * width * y + 4 * x
 						+ c];
 			}
-	width = origWidth;
-	height = origHeight;
 
-	std::cout << "b_origWidth: " << origWidth << "\n";
-	std::cout << "b_origHeight: " << origHeight << "\n";
-
-	return bricks2;
+	return outArr;
 }
 
-std::vector<unsigned char> makeWoodPowerOfTwo(size_t& u2, size_t& v2) {
-
+void texDimenPowerOfTwo() {
 	// Texture size must be power of two for the primitive OpenGL version this is written for. Find next power of two.
 	while (u2 < width)
 		u2 *= 2;
 	while (v2 < height)
 		v2 *= 2;
-	// Make power of two version of the image.
-	std::vector<unsigned char> wood2(u2 * v2 * 4);
-	unsigned origWidth = width;
-	unsigned origHeight = height;
+}
+void loadTexture(const std::string& filename) {
+	preArr.erase(preArr.begin(), preArr.end());
+	lodepng::decode(preArr, width, height, filename);
+	texDimenPowerOfTwo();
+}
 
-	//Dimensions here are effectively the cropping
-	for (size_t y = 0; y < height; y++)
-		for (size_t x = 0; x < width; x++)
-			for (size_t c = 0; c < 4; c++) {
-				wood2[4 * u2 * y + 4 * x + c] = wood[4 * width * y + 4 * x + c];
-			}
-	width = origWidth;
-	height = origHeight;
-
-	std::cout << "w_origWidth: " << origWidth << "\n";
-	std::cout << "w_origHeight: " << origHeight << "\n";
-
-	return wood2;
+void buildTexImg2D(std::vector<unsigned char> fullArr, int texArrPos) {
+	glBindTexture(GL_TEXTURE_2D, texName[texArrPos]);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, u2, v2, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+			&fullArr[0]);
 }
 
 void init() {
-	// The official code for "Setting Your Raster Position to a Pixel Location" (i.e. set up a camera for 2D screen)
 	glViewport(0, 0, 768, 768);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	// Make some OpenGL properties better for 2D and enable alpha channel.
 	glDisable(GL_CULL_FACE);
 	glDisable(GL_DEPTH_TEST);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -140,31 +115,19 @@ void init() {
 	glDisable(GL_ALPHA_TEST);
 
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-	glGenTextures(2, texName);
+	glGenTextures(NUM_OF_TEXTURES, texName);
 
 	/***********************************/
-	// Load file and decode image.
-	lodepng::decode(bricks, width, height, "colorfade.png");
-	// Make power of two version of the image.
-	std::vector<unsigned char> bricks2 = makeBricksPowerOfTwo(u2, v2);
-
-	glBindTexture(GL_TEXTURE_2D, texName[0]);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, 4, u2, v2, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-			&bricks2[0]);
+	loadTexture("colorfade.png");
+	std::vector<unsigned char> brickArr(u2 * v2 * 4);
+	brickArr = makePowerOfTwo(u2, v2, preArr, brickArr);
+	buildTexImg2D(brickArr, 0);
 
 	/***********************************/
-	// Load file and decode image.
-	lodepng::decode(wood, width, height, "wood.png");
-	// Make power of two version of the image.
-	std::vector<unsigned char> wood2 = makeWoodPowerOfTwo(u2, v2);
-
-	glBindTexture(GL_TEXTURE_2D, texName[1]);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, 4, u2, v2, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-			&wood2[0]);
+	loadTexture("wood.png");
+	std::vector<unsigned char> woodArr(u2 * v2 * 4);
+	woodArr = makePowerOfTwo(u2, v2, preArr, woodArr);
+	buildTexImg2D(woodArr, 1);
 
 	/***********************************/
 
@@ -190,7 +153,7 @@ void display(void) {
 //	glTranslatef(-2.0, -2.0, -4.6);
 //	glScalef(7.0, 7.0, 7.0);
 
-	glBindTexture(GL_TEXTURE_2D, texName[1]);
+	glBindTexture(GL_TEXTURE_2D, texName[0]);
 	glBegin(GL_QUADS);
 	glTexCoord2d(0, 0);
 	glVertex2f(0, 0);
